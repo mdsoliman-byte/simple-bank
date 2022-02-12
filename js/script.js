fcb9db3bbfba804bb61e6ac5633a3b258c5ec9c9
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-02-12 17:29:07 +0600

    depositit section compleat

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-document.getElementById('user-login-btn').addEventListener('click' , function(){
-    const userEmail = document.getElementById('user-email').value ;
-    const userPassword = document.getElementById('user-password').value;
-    
+const myPass = "shakil";
+const myUser = "shakil@gamil.com"
+document
+  .getElementById("user-login-btn")
+  .addEventListener("click", function () {
+    const userEmail = document.getElementById("user-email").value;
+    const userPassword = document.getElementById("user-password").value;
 
-    if( userEmail == "shakil@gamil.com" && userPassword == "shakil"){
-window.location.href = 'dashbord.html';
-    }else{
-        window.location.href = "404.html"
+    if (userEmail == myUser && userPassword == myPass ) {
+      window.location.href = "dashbord.html";
+    } else {
+      window.location.href = "404.html";
     }
-})
+  });
