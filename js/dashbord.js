e3ec0d68fa67d14aa3c838f5e5a02a708c8c6ccb
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-02-13 00:34:02 +0600

    adding Total Balance section

--- js/dashbord.js
+++ js/dashbord.js
@@ -1,8 +1,38 @@
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
-    const depositInput = document.getElementById("deposit-input").value;
-    document.getElementById("deposit-fild").innerText = depositInput;
+
+    // Update Deposite Ammount 
+    const depositInput = parseFloat(document.getElementById("deposit-input").value);
+
+    const depositPvalue = parseFloat(document.getElementById("deposit-fild").innerText);
+    // console.log(depositPvalue)
+    const updateAmmount = depositInput + depositPvalue
+    document.getElementById("deposit-fild").innerText = updateAmmount ;
+
+// Updata Account Balance 
+const privousBalance = parseFloat(document.getElementById('balance-fild').innerText);
+const updateBalance =  depositInput + privousBalance;
+console.log(updateBalance);
+document.getElementById('balance-fild').innerText = updateBalance;
+// Update Withdrawal Ammount
+// console.log(depositInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Clear input 
 
     document.getElementById("deposit-input").value= " ";
